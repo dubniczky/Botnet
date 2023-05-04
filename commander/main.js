7c20c90b359351076757b5c2589aa8c9901986b4
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2023-05-04 22:53:57 +0200

    Added basic commander server

---
+++ commander/main.js
@@ -0,0 +1,17 @@
+//!/usr/bin/env node
+
+const express = require('express')
+const uuid = require('uuid')
+
+const app = express()
+
+app.get('/command', (req, res) => {
+    res.send({
+        id: uuid.v4(),
+        command: 'ls',
+        time: 0,
+        signature: '-'
+    })
+})
+
+app.listen(3000, () => console.log('Commander listening on port 3000!'))
